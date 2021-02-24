30720485776095f504a38a9513c8824c6374c194
Kanyarat2912 <61747690+Kanyarat2912@users.noreply.github.com>
2021-02-25 00:37:23 +0700

    ------

--- ENG/delete_permission.php
+++ ENG/delete_permission.php
@@ -1,1 +1,15 @@
-adssdasdasd
+<?php
+include "connectpcr.php";
+$sql_Up = "UPDATE pcr_access_time SET at_request_status = 0 WHERE at_emp_code = '".$_GET["id"]."'";
+if($result_Up = mysqli_query($conn, $sql_Up)){
+	echo "<script>";
+	echo "alert('Successfully.');";
+	echo "</script>";
+	echo '<meta http-equiv=refresh content=0;URL=../INT/add_permission.php.php>';
+}else{
+	echo "<script>";
+	echo "alert('Error 1 : Please contact your system administrator for assistance.');";
+	echo "</script>";
+	echo '<meta http-equiv=refresh content=0;URL=../INT/add_permission.php.php>';
+}
+?>
